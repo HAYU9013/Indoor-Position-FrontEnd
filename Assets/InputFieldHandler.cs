9d6b77567580d56b9d4dca0d015a279d57bf0062
HAYU9013 <114861748+HAYU9013@users.noreply.github.com>
2023-11-02 20:22:40 +0800

    feat: 可以透過手動設定 target 讓地圖圖釘亮起來

---
+++ Assets/InputFieldHandler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputFieldHandler : MonoBehaviour
+{
+
+    public GameObject mapArea;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mapArea = GameObject.Find("MapArea");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var input = gameObject.GetComponent<InputField>();
+        input.onEndEdit.AddListener(SubmitName);
+
+    }
+
+    private void SubmitName(string arg0)
+    {
+        Debug.Log(arg0);
+        mapArea.GetComponent<MapHandler>().target = int.Parse(arg0); ;
+    }
+}
